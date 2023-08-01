9d67c73e631030434a6e770ab87ebd719d649e3a
brut4luk3 <lucasreinert96@gmail.com>
2023-08-01 16:57:34 -0300

    Funções de deletar e editar incorporadas

--- datas/urls.py
+++ datas/urls.py
@@ -6,5 +6,7 @@ app_name = 'datas'
 
 urlpatterns =[
     path('cadastrar/', views.cadastrar_feriados, name='cadastrar_feriados'),
-    path('deletar/', views.deletar_feriados, name='deletar_feriados'),
+    path('selecionar/', views.selecionar_feriados, name='selecionar_feriados'),
+    path('deletar/<int:feriados_id>/', views.deletar_feriados, name='deletar_feriados'),
+    path('editar/<int:feriados_id>/', views.editar_feriados, name='editar_feriados'),
 ]
--- datas/views.py
+++ datas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
 from datas.models import Feriados
@@ -23,6 +23,45 @@ def cadastrar_feriados(request):
 
         return redirect(reverse('datas:cadastrar_feriados'))
 
-def deletar_feriados(request):
+def selecionar_feriados(request):
+    feriados_cadastrados = request.user.feriados_set.all()
 
-    return render(request, 'deletar/deletar_feriados.html')
+    context = {
+        'feriados_cadastrados': feriados_cadastrados
+    }
+
+    return render(request, 'deletar/selecionar_feriados.html', context=context)
+
+def deletar_feriados(request, feriados_id):
+    if request.method == 'GET':
+
+        feriado = get_object_or_404(Feriados, pk=feriados_id)
+
+        context = {
+            'feriado': feriado
+        }
+
+        return render(request, 'deletar/deletar_feriados.html', context=context)
+
+    elif request.method == 'POST':
+
+        feriado_excluido = Feriados.objects.filter(id=feriados_id).delete()
+
+        return render(request, 'deletar/deletar_feriados.html')
+
+def editar_feriados(request, feriados_id):
+    if request.method == 'GET':
+
+        feriado = get_object_or_404(Feriados, pk=feriados_id)
+
+        context = {
+            'feriado': feriado
+        }
+
+        return render(request, 'editar/editar_feriados.html', context=context)
+
+    elif request.method == 'POST':
+
+        feriado_editado = Feriados.objects.filter(id=feriados_id).update()
+
+        return render(request, 'editar/editar_feriados.html')
