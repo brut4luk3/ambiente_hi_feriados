ec251e14c92d8c09fab15bb29665920cac0fa0b0
brut4luk3 <lucasreinert96@gmail.com>
2023-09-19 15:32:55 -0300

    API melhorada

--- ambiente_hi_feriados/settings.py
+++ ambiente_hi_feriados/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s7nxrv!l1r29vq2b@4jazhubql1g6#m3hicfi(ah)v-(z99aa3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['http://brut4luk3.pythonanywhere.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
