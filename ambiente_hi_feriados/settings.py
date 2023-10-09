71b957e2760835fc0cd99630eddbd50c116472a0
brut4luk3 <lucasreinert96@gmail.com>
2023-10-09 11:36:24 -0300

    commit final

--- ambiente_hi_feriados/settings.py
+++ ambiente_hi_feriados/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'ambiente_hi_feriados.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
